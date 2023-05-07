38ae276420e75fff8082023588f3bcdceb78ce3c
fxzer <wy3010099292@163.com>
2023-05-07 18:26:37 +0800

    feat: add word connect line

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,10 +5,16 @@
     <NavMenu :navs="navs" />
     <Arrow class="left-4 rotate-180 arrow-common" @click="switchHandler(Direction.prev)" />
     <Arrow class="right-4  arrow-common" @click="switchHandler(Direction.next)" />
+    <button class="absolute top-4 right-4" @click="themeToggle">
+    <svg   class=" text-slate-200"  v-show="isDark" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26a5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1z"/></svg>
+    <svg   class=" text-orange-400" v-show="!isDark" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 17q-2.075 0-3.538-1.463T7 12q0-2.075 1.463-3.538T12 7q2.075 0 3.538 1.463T17 12q0 2.075-1.463 3.538T12 17ZM1 13v-2h4v2H1Zm18 0v-2h4v2h-4Zm-8-8V1h2v4h-2Zm0 18v-4h2v4h-2ZM6.35 7.75L3.875 5.275l1.4-1.4L7.75 6.35l-1.4 1.4Zm12.375 12.375L16.25 17.65l1.4-1.4l2.475 2.475l-1.4 1.4ZM17.65 7.75l-1.4-1.4l2.475-2.475l1.4 1.4L17.65 7.75ZM5.275 20.125l-1.4-1.4L6.35 16.25l1.4 1.4l-2.475 2.475Z"/></svg>
+  </button>
   </div>
 </template>
 <script setup lang='ts'>
 import { useRoute, useRouter } from 'vue-router';
+import { useDark, useToggle } from '@vueuse/core'
+
 import NavMenu from './NavMenu.vue';
 import Arrow from '@/components/Arrow.vue';
 import Logo from '@/components/Logo.vue';
@@ -28,6 +34,12 @@ const switchHandler = (direction: Direction) => {
   let edgeNextPath = direction == Direction.prev ? '/' + navs[navs.length - 1] : '/' + navs[0]
   let nextPath = isEdge ? edgeNextPath : navs[nextIdx]
   currentRouter.push(nextPath)
+}
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+const themeToggle = () => {
+  toggleDark()
 }
 </script>
 <style scoped lang='scss'></style>
--- src/views/d/index.vue
+++ src/views/d/index.vue
@@ -1,5 +1,144 @@
 <template>
-   <div class="d">
-      <h1 class="text-3xl">d works !!!</h1>
-    </div>
+   <canvas ref="canvasC"></canvas>
 </template>
+<script lang="ts" setup>
+import { onMounted, ref, watchEffect, } from 'vue';
+import useMouse from '@/hooks/useMouse'
+import { useDark } from '@vueuse/core'
+
+const isDark = useDark()
+
+const canvasC = ref<HTMLCanvasElement>()
+const ctx = ref<CanvasRenderingContext2D | null>(null);
+const { x, y } = useMouse()
+let particlesArray: Particle[] = [];
+const mouse = {
+   x,
+   y,
+   radius: 200
+}
+/* 自定义例子颜色，数量，密度 */
+class Particle { //粒子
+   x: number;
+   y: number;
+   size: number;
+   baseX: number;
+   baseY: number;
+   density: number;
+
+   constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+      this.size = 4; //粒子大小
+      this.baseX = this.x; //粒子初始位置
+      this.baseY = this.y; //粒子初始位置
+      this.density = (Math.random() * 10) + 1; //粒子密度
+   }
+   draw() {
+      ctx.value?.beginPath();
+      ctx.value!.fillStyle = 'green';
+      ctx.value?.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.value?.closePath();
+      ctx.value?.fill();
+   }
+   update() {
+      let dx = mouse.x.value - this.x;
+      let dy = mouse.y.value - this.y;
+      let distance = Math.sqrt(dx * dx + dy * dy); // 粒子与鼠标的距离
+
+      //计算力度
+      let forceDirectionX = dx / distance;
+      let forceDirectionY = dy / distance;
+
+      let maxDistance = mouse.radius;
+      let force = (maxDistance - distance) / maxDistance;
+      let directionX = forceDirectionX * force * this.density;
+      let directionY = forceDirectionY * force * this.density;
+      if (distance < mouse.radius) { //在鼠标半径范围内的粒子
+         this.x -= directionX
+         this.y -= directionY
+      } else {//不在鼠标半径范围内的粒子
+         let speed = 20; //粒子回到初始位置的速度越大越慢
+         if (this.x !== this.baseX) {
+            let dx = this.x - this.baseX;
+            this.x -= dx / speed;
+         }
+         if (this.y !== this.baseY) {
+            let dy = this.y - this.baseY;
+            this.y -= dy / speed;
+         }
+      }
+   }
+}
+const fillText = () => {
+   /* 文字粒子 */
+   let cw = canvasC.value!.width;
+   let ch = canvasC.value!.height;
+   particlesArray = [];
+   let fontSize = 40;
+   let text = 'ABC'
+   let adjustX = Math.floor((cw - ctx.value!.measureText('A').width) / 3.5) - 400;
+   let adjustY = Math.floor((ch - fontSize * 10) / 4);
+   ctx.value!.font = `${fontSize}px Verdana`; //字体大小
+   ctx.value?.fillText(text, 0, fontSize - (0.2 * fontSize), cw); //让文字初始化在 canvas 最左上角
+   const textCoordinates = ctx.value?.getImageData(0, 0, cw, ch);
+   let { width: w = 0, height: h = 0, data = [] } = textCoordinates || {};
+   for (let x = 0; x < w; x++) {
+      for (let y = 0; y < h; y++) {
+         if (data[(y * 4 * w) + (x * 4) + 3] > 128) {
+            particlesArray.push(new Particle(x * 20 + adjustX, y * 20 + adjustY))
+         }
+      }
+   }
+}
+const initCanvas = () => {
+   if (canvasC.value) {
+      canvasC.value.width = window.innerWidth || 600;
+      canvasC.value.height = window.innerHeight || 600;
+      ctx.value = canvasC.value?.getContext('2d') as CanvasRenderingContext2D;   //获取canvas的上下文
+      fillText()
+   }
+}
+
+const strokePre = ref<string>('rgba(255,255,255,')
+
+//粒子连接线
+const connect = () => {
+   let opacity = 0.6;
+   for (let a = 0; a < particlesArray.length; a++) {
+      for (let b = a; b < particlesArray.length; b++) {
+         let dx = particlesArray[a].x - particlesArray[b].x;
+         let dy = particlesArray[a].y - particlesArray[b].y;
+         let distance = Math.sqrt(dx * dx + dy * dy);
+         let maxDistance = 30;
+         if (distance < maxDistance) {
+            opacity = parseFloat((1 - (distance / maxDistance)).toFixed(2));
+            ctx.value!.strokeStyle = `${strokePre.value}${opacity})`;
+            ctx.value?.beginPath();
+            ctx.value!.lineWidth = 1.8;
+            ctx.value?.moveTo(particlesArray[a].x, particlesArray[a].y);
+            ctx.value?.lineTo(particlesArray[b].x, particlesArray[b].y);
+            ctx.value?.stroke();
+         }
+      }
+   }
+}
+//动画
+const animate = () => {
+   ctx.value?.clearRect(0, 0, canvasC.value!.width, canvasC.value!.height);
+   for (let i = 0; i < particlesArray.length; i++) {
+      particlesArray[i].draw();
+      particlesArray[i].update();
+   }
+   connect()
+   requestAnimationFrame(animate);
+}
+watchEffect(() => {
+   strokePre.value =  isDark.value ? 'rgba(255,255,255,' : 'rgba(0,0,0,'
+})
+
+onMounted(() => {
+   initCanvas();
+   animate()
+})
+</script>
