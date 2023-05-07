605e10c68e9fe0762fad07c6724eedc31c1eaf2a
fxzer <wy3010099292@163.com>
2023-05-07 20:37:49 +0800

    fix: active nav styel bug

--- src/layout/NavMenu.vue
+++ src/layout/NavMenu.vue
@@ -1,10 +1,10 @@
+       
 <template>
   <div class="text-center absolute bottom-8 left-0 right-0 select-none">
     <router-link v-for="nav in navs" :key="nav" :to="'/' + nav" class="link inline-block text-xl  px-2  font-mono  ease-in-out   duration-500 group
-       text-zinc-400 hover:text-zinc-600 origin-bottom relative
-       dark:text-zinc-600   dark:hover:text-zinc-400" :class="{
-         'text-zinc-800   dark:text-zinc-200 scale-125': currentRoute.path === '/' + nav,
-       }">
+     hover:text-zinc-600 origin-bottom relative   dark:hover:text-zinc-400" :class=" 
+         currentRoute.path === ('/' + nav  ) ? 'text-zinc-800 dark:text-zinc-200 scale-125' : ' text-zinc-400 dark:text-zinc-600'
+       ">
       <span> {{ nav.toUpperCase() }}</span>
       <div
         class="preview w-20 h-20 origin-bottom-left -rotate-45 dark:border border-slate-100 absolute bottom-10 left-[14px] 
