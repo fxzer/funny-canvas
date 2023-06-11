511aa4b02477c355ab69bc44e982321f062f6eab
fxzer <wy3010099292@163.com>
2023-06-11 22:36:28 +0800

    feat: 优化粒子动画

--- src/views/j/index.vue
+++ src/views/j/index.vue
@@ -1,117 +1,126 @@
-<script setup lang='ts'>
-import { ref,  onMounted, onUnmounted, computed } from 'vue'
-import { useDark } from '@vueuse/core'
-const isDark = useDark()
-const canvasRef = ref<HTMLCanvasElement | null>(null)
-let width = 0
-let height = 0
-let ctx: CanvasRenderingContext2D
-let timer = 0
-const pointColor = computed(() => isDark.value ? '#fff' : '#000')
-const lineColor = computed(() => isDark.value ? 'rgba(255,255,255,' : 'rgba(0,0,0,')
-let grap: Graph
-onMounted(() => {
-  if (canvasRef.value) {
-    width = canvasRef.value.width = window.innerWidth || 600;
-    height = canvasRef.value.height = window.innerHeight || 600;
-    ctx = canvasRef.value?.getContext('2d') as CanvasRenderingContext2D;   //获取canvas的上下文
-  }
-  grap = new Graph()
-  grap.draw()
-  animate()
-})
-onUnmounted(() => {
-  if (timer)
-    cancelAnimationFrame(timer)
-})
-
-function randomNum(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-class Point { // 粒子
-  x: number
-  y: number
-  size: number
-  speedX: number
-  speedY: number
-  lastTime: number
-  constructor() {
-    this.size = 2 // 粒子大小
-    this.x = randomNum(0, width - this.size / 2)
-    this.y = randomNum(0, height - this.size / 2)
-    this.speedX = randomNum(-50, 50)
-    this.speedY = randomNum(-50, 50)
-    this.lastTime = 0
-  }
-
-  draw() {
-    if (this.lastTime) {
-      const now = Date.now()
-      const dt = (now - this.lastTime) / 1000
-      if (this.x > width || this.x < 0)
-        this.speedX = -this.speedX
-
-      if (this.y > height || this.y < 0)
-        this.speedY = -this.speedY
-
-      this.x += this.speedX * dt
-      this.y += this.speedY * dt
-    }
-    ctx?.beginPath()
-    ctx!.fillStyle =  pointColor.value
-    ctx?.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-    ctx?.closePath()
-    ctx?.fill()
-    this.lastTime = Date.now()
-  }
-}
-
-class Graph {
-  pointCount: number // 点的数量
-  maxDistance: number // 点之间连线的最大距离
-  points: Point[]
-  constructor() {
-    this.pointCount = 100
-    this.maxDistance = 200
-    this.points = new Array(this.pointCount).fill(0).map(() => new Point())
-  }
-
-  draw() {
-    for (let i = 0; i < this.points.length; i++) {
-      const p1 = this.points[i]
-      p1.draw()
-      for (let j = i + 1; j < this.points.length; j++) {
-        const p2 = this.points[j]
-        const dx = p1.x - p2.x
-        const dy = p1.y - p2.y
-        const distance = Math.sqrt(dx * dx + dy * dy)
-        if (distance > this.maxDistance)
-          continue
-        ctx?.beginPath()
-        // ctx!.strokeStyle = `rgba(255,255,255,${(this.maxDistance - distance) / this.maxDistance})`
-        let alpha = (this.maxDistance - distance) / this.maxDistance
-        ctx!.strokeStyle = `${lineColor.value}${alpha})`
-        ctx!.lineWidth = 1
-        ctx?.moveTo(p1.x, p1.y)
-        ctx?.lineTo(p2.x, p2.y)
-        ctx?.closePath()
-        ctx?.stroke()
-      }
-    }
-  }
-}
-
-function animate() {
-  ctx?.clearRect(0, 0, width, height)
-  if (timer)
-    cancelAnimationFrame(timer)
-  grap.draw()
-  timer = requestAnimationFrame(animate)
-}
-</script>
-
 <template>
-  <canvas ref="canvasRef" :class="isDark ? ' bg-black' : ''" />
+  <div ref="wrap" class="w-full h-full">
+    <canvas ref="canvas"></canvas>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+
+<script setup>
+
+import { ref, onMounted, onUnmounted } from 'vue';
+const wrap = ref(null);
+const canvas = ref(null);
+const ctx = ref(null);
+const particles = ref([]);
+const particleRadius = 2;
+const attrtchDistance = 180;
+const connectionDistance = 120;
+const connectionOpacity = 0.2;
+let mouseX = 0;
+let mouseY = 0;
+
+const initializeCanvas = () => {
+  const { width, height } = wrap.value.getBoundingClientRect();
+  canvas.value.width = width;
+  canvas.value.height = height;
+};
+
+const createParticles = () => {
+  for (let i = 0; i < 100; i++) {
+    const particle = {
+      x: Math.random() * canvas.value.width,
+      y: Math.random() * canvas.value.height,
+      dx: Math.random() * 2 - 1,
+      dy: Math.random() * 2 - 1
+    };
+    particles.value.push(particle);
+  }
+};
+
+const animateParticles = () => {
+  ctx.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
+  particles.value.forEach(particle => {
+    
+    const distance = Math.sqrt(
+      Math.pow(particle.x - mouseX, 2) +
+      Math.pow(particle.y - mouseY, 2)
+    );
+    if (distance < attrtchDistance   ) {
+      const opacity = 1 - distance / attrtchDistance;
+      ctx.value.beginPath();
+      ctx.value.moveTo(particle.x, particle.y);
+      ctx.value.lineTo(mouseX, mouseY);
+      ctx.value.strokeStyle = `rgba(0, 0, 0, ${opacity * connectionOpacity})`;
+      ctx.value.stroke();
+      ctx.value.closePath();
+    }
+    //如果<180并且大于120，则吸引
+    if (distance < attrtchDistance && distance > connectionDistance) {
+      particle.x += (mouseX - particle.x) * 0.02;
+      particle.y += (mouseY - particle.y) * 0.02;
+    }else{
+      particle.x += particle.dx;
+      particle.y += particle.dy;
+    }
+
+    if (particle.x < particleRadius || particle.x > canvas.value.width - particleRadius) {
+      particle.dx *= -1;
+    }
+
+    if (particle.y < particleRadius || particle.y > canvas.value.height - particleRadius) {
+      particle.dy *= -1;
+    }
+
+    ctx.value.beginPath();
+    ctx.value.arc(particle.x, particle.y, particleRadius, 0, Math.PI * 2);
+    ctx.value.fillStyle = '#000';
+    ctx.value.fill();
+    ctx.value.closePath();
+
+ 
+    particles.value.forEach(otherParticle => {
+      if (particle !== otherParticle) {
+        const dp = Math.sqrt(
+          Math.pow(particle.x - otherParticle.x, 2) +
+          Math.pow(particle.y - otherParticle.y, 2)
+        );
+
+        if (dp < connectionDistance ) {
+          const opacity = 1 - dp / connectionDistance;
+          ctx.value.beginPath();
+          ctx.value.moveTo(particle.x, particle.y);
+          ctx.value.lineTo(otherParticle.x, otherParticle.y);
+          ctx.value.strokeStyle = `rgba(0, 0, 0, ${opacity * connectionOpacity})`;
+          ctx.value.stroke();
+          ctx.value.closePath();
+        }
+      }
+    });
+
+    
+  });
+
+  requestAnimationFrame(animateParticles);
+};
+
+const handleMouseMove = (event) => {
+  const rect = wrap.value.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+};
+
+onMounted(() => {
+  canvas.value = canvas.value;
+  ctx.value = canvas.value.getContext('2d');
+  initializeCanvas();
+  createParticles();
+  animateParticles();
+  canvas.value.addEventListener('mousemove', handleMouseMove);
+});
+
+onUnmounted(() => {
+  cancelAnimationFrame(animateParticles);
+  canvas.value?.removeEventListener('mousemove', handleMouseMove);
+});
+</script>
+
