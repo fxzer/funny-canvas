8dd302df26a7afec96a75e9ed74c75a943958410
fxzer <fxzer8888@163.com>
2024-01-09 21:26:47 +0800

    feat: add u demo

--- src/views/u/index.vue
+++ src/views/u/index.vue
@@ -1,7 +1,116 @@
+<script lang="ts" setup>
+const { canvasRef, context, width, height } = useCanvas()
+const particles: Particle[] = []
+const numbers: number[][][] = []
+let count = 60
+let index = 0
+
+class Particle {
+  x: number
+  y: number
+  vx: number
+  vy: number
+  g: number
+  ty: number
+  opt: number
+  fall: boolean
+  color: string
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+    this.vx = (Math.random() - 0.5) * 4
+    this.vy = Math.random() * 3 + 5
+    this.g = this.vy
+    this.ty = 0.1
+    this.opt = 0
+    this.fall = false
+    this.color = `hsla(${index * 30},80%,50%,`
+  }
+
+  update() {
+    if (this.fall) {
+      if (this.y >= height.value) {
+        this.g *= Math.random() * 0.8
+        this.vy = -this.g
+      }
+      this.x += this.vx
+      this.y += this.vy
+      this.vy += this.ty
+    }
+    this.opt += 0.01
+  }
+
+  draw() {
+    if (!context.value)
+      return
+    context.value.beginPath()
+    context.value.arc(this.x, this.y, 5, 0, Math.PI * 2, false)
+    context.value.fillStyle = `${this.color + this.opt})`
+    context.value.fill()
+  }
+}
+
+function change(i: number) {
+  for (const j in numbers[i]) {
+    const p = numbers[i][j]
+    // console.log('[ p ]-57', p)
+    particles.push(new Particle(p[0], p[1]))
+  }
+}
+
+function init() {
+  if (!context.value)
+    return
+  for (let i = 0; i <= 10; i++) {
+    context.value.font = '40px Arial'
+    const w = context.value.measureText(`${i}`).width | 0
+    context.value.fillText(`${i}`, 0, 40)
+    const data = context.value.getImageData(0, 0, w, 41).data
+    const len = data.length
+    const tdata = []
+    for (let j = 0; j < len / 4; j++) {
+      if (data[j * 4 + 3] !== 0) {
+        const x = j % w | 0
+        const y = j / w | 0
+        tdata.push([x * 20 + (width.value - w * 20) / 2, y * 20 + (height.value - 40 * 20) / 2 - 200])
+      }
+    }
+    numbers.push(tdata)
+    context.value.clearRect(0, 0, 40, 41)
+  }
+}
+
+function animate() {
+  if (!context.value)
+    return
+  context.value.fillStyle = 'rgba(0,0,0,0.2)'
+  context.value.fillRect(0, 0, width.value, height.value)
+  count++
+  particles.forEach((p, i) => {
+    p.update()
+    p.draw()
+    if (Math.abs(p.g) < 2)
+      particles.splice(i, 1)
+
+    if (count === 60)
+      p.fall = true
+  })
+  if (count === 120) {
+    change(10 - index)
+    count = 0
+    index++
+    index = index % numbers.length
+  }
+  requestAnimationFrame(animate)
+}
+
+onMounted(() => {
+  init()
+  animate()
+})
+</script>
+
 <template>
-  <div class="u">
-    <h1 class="text-3xl">
-      u works !!!
-    </h1>
-  </div>
+  <canvas ref="canvasRef" />
 </template>
