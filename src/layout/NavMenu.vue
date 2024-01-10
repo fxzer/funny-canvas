23e906efeaa57a38d70e152868aaffce576945e1
fxzer <fxzer8888@163.com>
2024-01-10 08:40:11 +0800

    style: update nav style

--- src/layout/NavMenu.vue
+++ src/layout/NavMenu.vue
@@ -7,22 +7,21 @@ defineProps({
 })
 
 const currentRoute = useRoute()
-const isActive = (path: string) => currentRoute.path === path
+const activeClass = (path: string) => currentRoute.path === path ? 'text-green scale-125' : 'text-zinc'
 </script>
 
 <template>
-  <div class="text-center absolute bottom-8 left-0 right-0 select-none z-50">
+  <div text-center absolute bottom-8 left-0 right-0 select-none z-50>
     <router-link
-      v-for=" nav in navs" :key="nav.id" :to="`/${nav.label}`" class="link inline-block text-xl font-mono ease-in-out duration-500 group
-   hover:text-zinc-600 origin-bottom relative  dark:hover:text-zinc-400"
-      :class="isActive(`/${nav.label}`) ? 'text-black dark:text-white scale-125' : 'text-zinc-400 dark:text-zinc-600'
-      "
+      v-for=" nav in navs" :key="nav.id" :to="`/${nav.label}`"
+      class="link inline-block text-xl font-mono ease-in-out duration-200 group hover:text-zinc-600 origin-bottom relative  dark:hover:text-zinc-200"
+      :class="activeClass(`/${nav.label}`)"
     >
       <span class="letter px-2" @mouseenter="nav.ishover = true" @mouseleave="nav.ishover = false">
         {{ nav.label.toUpperCase() }}</span>
       <div
-        class="wh-20 origin-bottom-left -rotate-45 dark:border border-slate-100 absolute bottom-8 left-[14px]
-     overflow-hidden -z-10 ease-in-out duration-500 transition-all" :class="nav.ishover ? 'block' : 'hidden'"
+        border="dark:~ slate-200" class="wh-20 origin-bottom-left -rotate-45 absolute bottom-8 left-[14px] overflow-hidden -z-10 ease-in-out duration-500 transition-all"
+        :class="nav.ishover ? 'block' : 'hidden'"
       >
         <img :src="nav.src" class="wh-full rotate-45">
       </div>
